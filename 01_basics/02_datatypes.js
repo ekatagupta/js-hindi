8b453c1441db1ed53ac2ab394f740ddb5835bc49
Ekata Gupta <ekata8077@gmail.com>
2025-02-20 12:47:43 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)// we r using nodejs , not browser
+
+console.log(3+3)
+
+console.log("ekata")
+
+
+let name = "ekata"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint = for big number
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+// // symbol => unique
+
+
+
+//object
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);// object
