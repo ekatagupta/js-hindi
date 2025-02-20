78ffbc291eff8428c82d77d0d4a80208faed2ed6
Ekata Gupta <ekata8077@gmail.com>
2025-02-20 12:47:54 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "ekata@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+//accountId = 2//
+
+accountEmail ="hc@hc.com"
+accountPassword = "21212121"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
