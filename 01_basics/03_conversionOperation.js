069d21b6d868b34e9d1e913c84414b47dae9b645
Ekata Gupta <ekata8077@gmail.com>
2025-02-21 10:16:20 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,18 +1,18 @@
-let score = true
+//let score = true
 
 //console.log(typeof score);
 //console.log(typeof (score));
 
-let valueInNumber = Number (score)
+//let valueInNumber = Number (score)
 //console.log(valueInNumber);
 
 //"33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = "ekata"
+//let isLoggedIn = "ekata"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsLoggedIn);
 
 // 1=> true; 0 => false
@@ -20,6 +20,50 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 //"ekata" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
+//let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+
+
+
+
+// ********************************** Operations *******************************************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2*3);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 =" hitesh"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+
+//use code in only with parenthesis
+
+// console.log(+true);
+
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+
+
+// let gameCounter =100
+// ++gameCounter;
+// console.log(gameCounter);
+
+
