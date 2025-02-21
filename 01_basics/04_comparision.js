b5cdeafb12758031eef4e8865f2df2fd0b3715d3
Ekata Gupta <ekata8077@gmail.com>
2025-02-21 10:16:37 +0000

    conversion operation

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1); //t
+// console.log(2 >= 1);//t
+// console.log(2 < 1);//f
+// console.log(2 == 1);//f
+// console.log(2 != 1);//t
+
+
+// console.log("2" > 1);//t
+// console.log("02" > 1);//t
+
+
+// console.log(null > 0);//f
+// console.log(null == 0);//f
+// console.log(null >=0);//t
+                                   //most cases m is trah ke comparision ko avoid hi krte hn   
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  strict check
+
+// console.log("2" === 2);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Primitive
+
+// 7 types : string, number,Boolean , null, undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+constanotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 323456799n
+
+
+
+
+// Reference type/ Non primitive - Array, objects, functions
+
+
+const heros =["shaktiman","naagraj","doga"];
+    let myObj = {
+        name: "ekata",
+        age: 22,
+    }
+
+
+
+    // function as variable 
+    const myfunction = function(){
+        console.log("hello world");
+    }
+
+  console.log(typeof bigNumber);  
